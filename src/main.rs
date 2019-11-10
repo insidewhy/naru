545c3a4d0d50755469ae34d12c84b3cc447a768b
James Pike <jpike@chilon.net>
2019-11-11 01:09:18 +0800

    use xdg to find config file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 extern crate libc;
+extern crate xdg;
 use std::io;
 mod tty;
 
 const TTY_PATH: &str = "/dev/tty";
+
+fn load_config() -> io::Result<()> {
+  let xdg_dirs = xdg::BaseDirectories::new()?;
+  let cfg_file = xdg_dirs.find_config_file("toss.toml");
+
+  match cfg_file {
+    // TODO: return default config
+    None => println!("no config"),
+    // TODO: parse config file
+    Some(v) => println!("{}", v.to_str().unwrap()),
+  };
+
+  Ok(())
+}
 
 fn match_input() -> io::Result<()> {
   let mut choices: Vec<String> = Vec::new();
@@ -19,13 +34,12 @@ fn match_input() -> io::Result<()> {
   let terminal = tty::Tty::init(&TTY_PATH)?;
 
   // TODO: enable filtering etc.
-  // for choice in choices {
-  //   println!("{}", choice);
-  // }
+  // for choice in choices { println!("{}", choice); }
 
   terminal.reset()
 }
 
 fn main() {
+  load_config().unwrap();
   match_input().unwrap();
 }
