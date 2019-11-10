3540b634f62d280032741b4ea518f6e217740416
James Pike <jpike@chilon.net>
2019-11-10 21:08:51 +0800

    finish tty init/reset functions

--- src/main.rs
+++ src/main.rs
@@ -12,100 +12,24 @@ fn main() {
         if n == 0 {
           break;
         }
-        choices.push(input.trim().clone().to_string());
+        choices.push(input.trim().to_string());
         input.clear();
       }
       Err(error) => println!("error: {}", error),
     }
   }
 
-  // let stdout_fd;
-  // unsafe {
-  //   let var = libc::fopen(
-  //     CString::new("/dev/tty").unwrap().as_ptr(),
-  //     CString::new("r+").unwrap().as_ptr()
-  //   );
-  //   stdout_fd = libc::dup(1);
-  //   libc::dup2(libc::fileno(var), libc::STDIN_FILENO);
-  //   libc::dup2(libc::fileno(var), libc::STDOUT_FILENO);
-  // }
-
   let tty_path = "/dev/tty";
-  let terminal = tty::Tty::init(& tty_path);
-  match terminal {
-    Ok(_) => (),
+  let terminal = match tty::Tty::init(& tty_path) {
+    Ok(t) => t,
     Err(_) => panic!("Could not initialise terminal")
-  }
+  };
 
   // TODO: enable filtering etc.
-  for choice in choices {
-    println!("{}", choice);
-  }
+  // for choice in choices {
+  //   println!("{}", choice);
+  // }
 
-  /*
-  let mut idx: i32 = 0;
-  for (_idx, choice) in choices.iter().enumerate() {
-    if idx == _idx as i32 {
-      window.color_set(COLOR_MAGENTA);
-    }
-    else {
-      window.color_set(0);
-    }
-    window.printw(&choice);
-    window.printw("\n");
-  }
-
-  loop {
-    match window.getch() {
-      Some(val) => {
-        match val {
-          Input::Character('n') | Input::Character('j') => {
-            idx += 1;
-            let n_choices = choices.len() as i32;
-            if idx >= n_choices {
-              idx = n_choices - 1;
-            }
-            else {
-              if idx > 0 {
-                window.color_set(0);
-                window.mvprintw(idx - 1, 0 as i32, &choices[(idx - 1) as usize]);
-              }
-              window.color_set(COLOR_MAGENTA);
-              window.mvprintw(idx, 0 as i32, &choices[idx as usize]);
-            }
-          },
-          Input::Character('e') | Input::Character('k') => {
-            idx -= 1;
-            if idx < 0 {
-              idx = 0;
-            }
-            else {
-              let n_choices = choices.len() as i32;
-              if idx < n_choices - 1 {
-                window.color_set(0);
-                window.mvprintw(idx + 1, 0 as i32, &choices[(idx + 1) as usize]);
-              }
-              window.color_set(COLOR_MAGENTA);
-              window.mvprintw(idx, 0 as i32, &choices[idx as usize]);
-            }
-          },
-          Input::KeyEnter | Input::Character('\n') | Input::Character('c') => {
-            break;
-          },
-          _ => break,
-        }
-      },
-      None => (),
-    }
-  }
-  endwin();
-
-  let mut file = unsafe { File::from_raw_fd(stdout_fd) };
-  match file.write_all(choices[idx as usize].as_bytes()) {
-    Ok(_) => exit(0),
-    Err(_) => exit(1),
-  }
-  */
-
+  terminal.reset();
   exit(0);
 }
--- src/tty.rs
+++ src/tty.rs
@@ -1,35 +1,51 @@
 extern crate termios;
 use std::ffi::CString;
 use std::io;
-use self::termios::Termios;
-use libc;
+use std::io::{Error, ErrorKind};
+use self::termios::{Termios, ICRNL, ICANON, ECHO, ISIG, TCSANOW, tcsetattr};
+use libc::{setvbuf, _IOFBF};
 
 pub struct Tty {
-	fdin: i32,
-	fout: * const libc::FILE,
-	original_termios: Termios,
-	fg_color: i32,
-	max_width: i32,
-	max_height: i32
+  fdin: i32,
+  fout: * mut libc::FILE,
+  original_termios: Termios,
+  fg_color: i32,
+  max_width: i32,
+  max_height: i32,
 }
 
 impl Tty {
   pub fn init(tty_path: &str) -> io::Result<Tty> {
-
     let tty_filename_c = CString::new(tty_path)?;
     let fdin = unsafe { libc::open(tty_filename_c.as_ptr(), libc::O_RDONLY) };
+    let fout = unsafe { libc::fopen(tty_filename_c.as_ptr(), CString::new("w")?.as_ptr()) };
+
     let original_termios = Termios::from_fd(fdin)?;
 
-    let tty = Tty {
-      fdin: fdin,
-      fout: unsafe { libc::fopen(tty_filename_c.as_ptr(), CString::new("w")?.as_ptr()) },
-      fg_color: 9,
-      original_termios,
-      // TODO: query for these
-      max_width: 80,
-      max_height: 25,
-    };
+    let mut termios_copy = original_termios.clone();
+    termios_copy.c_iflag &= !(ICRNL);
+    termios_copy.c_lflag &= !(ICANON | ECHO | ISIG);
+    tcsetattr(fdin, TCSANOW, &termios_copy)?;
 
-    Ok(tty)
+    let err = unsafe { setvbuf(fout, std::ptr::null_mut(), _IOFBF, 4096) };
+    if err != 0 {
+      return Err(Error::new(ErrorKind::Other, "Could not setvbuf"));
+    }
+
+    Ok(
+      Tty {
+        fdin,
+        fout,
+        fg_color: 9,
+        original_termios,
+        // TODO: query for these
+        max_width: 80,
+        max_height: 25,
+      }
+    )
+  }
+
+  pub fn reset(&self) -> () {
+    tcsetattr(self.fdin, TCSANOW, &self.original_termios);
   }
 }
