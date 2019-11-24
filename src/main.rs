65614289260635a9229b82a89e600a8e1725c071
James Pike <jpike@chilon.net>
2019-11-24 13:14:53 +0800

    refactor selector logic into struct

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,12 @@
 mod config;
+mod selector;
 mod tty;
 use config::{load_config, Config};
-use std::ffi::CStr;
-use std::{
-  io,
-  io::{Error, ErrorKind},
-};
+use selector::Selector;
+use std::io;
 use tty::Tty;
 
 const TTY_PATH: &str = "/dev/tty";
-
-fn draw_matches(
-  terminal: &mut Tty,
-  choices: &Vec<String>,
-  height: u16,
-  selected: usize,
-) -> io::Result<()> {
-  let line_count = std::cmp::min(height as usize, choices.len() + 1);
-
-  for line_idx in 0..line_count - 1 {
-    terminal.newline()?;
-
-    if line_idx == selected {
-      terminal.set_invert()?;
-    }
-
-    terminal.print(choices[line_idx].as_str())?;
-
-    if line_idx == selected {
-      terminal.set_normal()?;
-    }
-  }
-
-  // move to the "top"
-  terminal.move_up((line_count - 1) as i32)?;
-  terminal.set_normal()?;
-  terminal.set_col(0)?;
-  terminal.print("> ")?;
-  terminal.clearline()?;
-
-  Ok(())
-}
-
-fn selector<'a>(
-  mut terminal: &mut Tty,
-  conf: &Config,
-  choices: &'a Vec<String>,
-) -> io::Result<&'a String> {
-  let height = std::cmp::min(conf.window.height, terminal.max_height);
-
-  // index of selected item
-  let mut selected = 0;
-  let mut criteria = String::new();
-
-  draw_matches(&mut terminal, &choices, height, selected)?;
-  terminal.flush();
-
-  let input_reader = terminal.get_reader();
-
-  loop {
-    let data = input_reader.read()?;
-    if data[0] == 0 {
-      // signal interrupt, redraw screen in case it was WINCH
-      draw_matches(&mut terminal, &choices, height, selected)?;
-      terminal.print(&criteria)?;
-      terminal.flush();
-    } else {
-      let str_ptr_result = unsafe { CStr::from_ptr(data.as_ptr() as *mut i8) }.to_str();
-      match str_ptr_result {
-        Ok(input) => {
-          if input == "\r" || input == "\n" {
-            break;
-          }
-
-          let mut chars = input.chars();
-          let first_char = chars.next();
-          if first_char.is_none() {
-            continue;
-          }
-
-          if first_char.unwrap().is_ascii_control() {
-            // TODO: lookup mapping in configuration, remove hard-coded mappings
-            if input == "\x1b[A" || input == "\x1bOA" {
-              if selected > 0 {
-                selected -= 1;
-                draw_matches(&mut terminal, &choices, height, selected)?;
-                terminal.print(&criteria)?;
-                terminal.flush();
-              }
-            } else if input == "\x1b[B" || input == "\x1bOB" {
-              if selected + 1 < choices.len() {
-                selected += 1;
-                draw_matches(&mut terminal, &choices, height, selected)?;
-                terminal.print(&criteria)?;
-                terminal.flush();
-              }
-            }
-          } else {
-            criteria.push_str(input);
-            terminal.print(input)?;
-            terminal.flush();
-          }
-        }
-        Err(_) => {
-          return Err(Error::new(ErrorKind::Other, "Could not convert string"));
-        }
-      }
-    }
-  }
-
-  Ok(&choices[selected])
-}
 
 fn match_input(conf: &Config) -> io::Result<()> {
   let mut choices: Vec<String> = Vec::new();
@@ -127,7 +23,10 @@ fn match_input(conf: &Config) -> io::Result<()> {
   let mut terminal = Tty::new(&TTY_PATH)?;
   terminal.set_no_wrap()?;
 
-  let result = selector(&mut terminal, &conf, &choices);
+  let result = {
+    let mut selector = Selector::new(&mut terminal, &choices, &conf);
+    selector.get_match()
+  };
 
   terminal.set_wrap()?;
   terminal.set_col(0)?;
---
+++ src/selector.rs
@@ -0,0 +1,127 @@
+use crate::config::Config;
+use crate::tty::Tty;
+
+use std::ffi::CStr;
+use std::{
+  io,
+  io::{Error, ErrorKind},
+};
+
+pub(crate) struct Selector<'a, 'b> {
+  // inputs
+  terminal: &'a mut Tty,
+  choices: &'b Vec<String>,
+  conf: &'b Config,
+
+  // min of terminal height or config height
+  height: u16,
+  selected: usize,
+  criteria: String,
+}
+
+impl<'a, 'b> Selector<'a, 'b> {
+  pub fn new(
+    terminal: &'a mut Tty,
+    choices: &'b Vec<String>,
+    conf: &'b Config,
+  ) -> Selector<'a, 'b> {
+    let max_height = terminal.max_height;
+
+    Selector {
+      terminal,
+      choices,
+      conf,
+      height: std::cmp::min(conf.window.height, max_height),
+      selected: 0,
+      criteria: String::new(),
+    }
+  }
+
+  pub fn get_match(&mut self) -> io::Result<&'b String> {
+    self.draw_matches()?;
+    self.terminal.flush();
+
+    let input_reader = self.terminal.get_reader();
+
+    loop {
+      let data = input_reader.read()?;
+      if data[0] == 0 {
+        // signal interrupt, redraw screen in case it was WINCH
+        self.redraw()?;
+      } else {
+        let str_ptr_result = unsafe { CStr::from_ptr(data.as_ptr() as *mut i8) }.to_str();
+        match str_ptr_result {
+          Ok(input) => {
+            if input == "\r" || input == "\n" {
+              break;
+            }
+
+            let mut chars = input.chars();
+            let first_char = chars.next();
+            if first_char.is_none() {
+              continue;
+            }
+
+            if first_char.unwrap().is_ascii_control() {
+              // TODO: lookup mapping in configuration, remove hard-coded mappings
+              if input == "\x1b[A" || input == "\x1bOA" {
+                if self.selected > 0 {
+                  self.selected -= 1;
+                  self.redraw()?;
+                }
+              } else if input == "\x1b[B" || input == "\x1bOB" {
+                if self.selected + 1 < self.choices.len() {
+                  self.selected += 1;
+                  self.redraw()?;
+                }
+              }
+            } else {
+              self.criteria.push_str(input);
+              self.terminal.print(input)?;
+              self.terminal.flush();
+            }
+          }
+          Err(_) => {
+            return Err(Error::new(ErrorKind::Other, "Could not convert string"));
+          }
+        }
+      }
+    }
+
+    Ok(&self.choices[self.selected])
+  }
+
+  fn redraw(&mut self) -> io::Result<()> {
+    self.draw_matches()?;
+    self.terminal.print(&self.criteria)?;
+    self.terminal.flush();
+    Ok(())
+  }
+
+  fn draw_matches(&mut self) -> io::Result<()> {
+    let line_count = std::cmp::min(self.height as usize, self.choices.len() + 1);
+
+    for line_idx in 0..line_count - 1 {
+      self.terminal.newline()?;
+
+      if line_idx == self.selected {
+        self.terminal.set_invert()?;
+      }
+
+      self.terminal.print(self.choices[line_idx].as_str())?;
+
+      if line_idx == self.selected {
+        self.terminal.set_normal()?;
+      }
+    }
+
+    // move to the "top"
+    self.terminal.move_up((line_count - 1) as i32)?;
+    self.terminal.set_normal()?;
+    self.terminal.set_col(0)?;
+    self.terminal.print("> ")?;
+    self.terminal.clearline()?;
+
+    Ok(())
+  }
+}
